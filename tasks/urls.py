2d6586276afc709c1b778ced43f253c73da3de1d
Abdul-Salam15 <abdulsalamadebayo0410@gmail.com>
2025-07-27 17:35:55 +0100

    Fixed the bug of taskee to tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 #The app_name variable is built in and should not be changed, because Django specifically looks for the variable app_name
-app_name = "taskee"
+app_name = "tasks"
 urlpatterns = [
     path("", views.index, name = "index"),
     path("add/", views.add, name = "add")
