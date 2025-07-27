475c2749b65d071a11da4810f17c60e08f8be095
Abdul-Salam15 <abdulsalamadebayo0410@gmail.com>
2025-07-27 17:53:08 +0100

    Added a class for inputing form data

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,9 @@
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task")
 
 tasks = ["Learn Django", "IBM skills guide", "BCG X Data science cource"]
 def index (request):
@@ -10,5 +13,5 @@ def index (request):
 
 def add (request):
     return render(request, "tasks/add.html",{
-
+        "form": NewTaskForm()
      })
