1b286b506f3627b91dc05f4d4f3cab3133920fcb
Abdul-Salam15 <abdulsalamadebayo0410@gmail.com>
2025-07-27 13:41:08 +0100

    Manually added tasks to the page

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+tasks = ["Learn Django", "IBM skills guide", "BCG X Data science cource"]
+def index (request):
+    return  render(request, "tasks/index.html", {
+        "tasks":tasks
+    })
