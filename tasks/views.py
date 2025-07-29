12ad0153e9fb9babe65569a91ec050f8ac415e03
Abdul-Salam15 <abdulsalamadebayo0410@gmail.com>
2025-07-29 22:41:48 +0100

    Added request sessions in the views

--- tasks/views.py
+++ tasks/views.py
@@ -6,10 +6,11 @@ from django.urls import reverse
 class NewTaskForm(forms.Form):
     task = forms.CharField(label = "New Task")
 
-tasks = []
 def index (request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return  render(request, "tasks/index.html", {
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 
 def add (request):
@@ -17,7 +18,7 @@ def add (request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
            task = form.cleaned_data["task"]
-           tasks.append(task)
+           request.session["tasks"] += [task]
            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
@@ -26,4 +27,4 @@ def add (request):
 #If the request is GET
     return render(request, "tasks/add.html",{
         "form": NewTaskForm()
-     }) 
+     })
